aa49f422810343ef482d20fed3249e104bd921b7
Alejandro Vaquera Lopez <alejandrovaqueralopez608@gmail.com>
2022-06-19 22:09:12 -0700

    feat:mongodb connection connected && loggin basic API crud ready

--- server.js
+++ server.js
@@ -1,30 +1,38 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const mongoose = require("mongoose");
+require('dotenv').config();
+const userRoutes = require("./app/routes/user");
  
 const app = express();
 
-var corsOptions = {
+/*var corsOptions = {
   origin: "*"
 };
 
-app.use(cors(corsOptions));
+app.use(cors(corsOptions));*/
 
 // parse requests of content-type - application/json
 app.use(bodyParser.json());
 
 // parse requests of content-type - application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }));
+//app.use(bodyParser.urlencoded({ extended: true }));
 
-//const db = require("./app/models");
-//const Role = db.role;
-// var express = require("express"), 
-router = express.Router();
+//db conection
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => console.log("Correctly connected to MONGODB"))
+  .catch((error) => console.error(error));
+
+//router
+app.use("/api",userRoutes);//user endpoint
+
 
 // simple route
 app.get("/", (req, res) => {
   res.json({message:"Hi this is the backend"});
 });
+
 
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
